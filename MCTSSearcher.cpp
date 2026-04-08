78c3408644e823e34c4b2adb7898ecf6660e514b
Aakash Kumar <aakashinthesky@gmail.com>
2026-04-08 15:02:46 -0400

    feat: implement array based mcts (without rollouts)

--- MCTSSearcher.cpp
+++ MCTSSearcher.cpp
@@ -3,3 +3,127 @@
 //
 
 #include "MCTSSearcher.h"
+
+#include "GameState.h"
+
+
+int MCTSSearcher::search(Hex boardState, bool isRED) {
+    parents[0] = -1; // root node has no parent
+    nextFree = 1;
+    isREDs[0] = isRED; // set this based on starting condition
+
+    mainLoop(boardState);
+
+    int childStart = childrenStarts[0];
+    int childEnd = childrenEnds[0];
+    int bestChildNode = childrenStarts[0];
+    double bestChildValue = -99999;
+    for (int child = childStart; child <= childEnd; child++) {
+        double childQ = this->returnSums[child] / (0.0001 + this->visitCounts[child]);
+        if (childQ > bestChildValue) {
+            bestChildValue = childQ;
+            bestChildNode = child;
+        }
+    }
+
+    return childActions[bestChildNode];
+}
+
+void MCTSSearcher::expand(int parent, GameState boardState) {
+    if (expandeds[parent] || terminals[parent]) {
+        return;
+    }
+
+    expandeds[parent] = true;
+    terminals[parent] = boardState.isTerminal();
+    if (terminals[parent]) {
+        return;
+    }
+    //todo: set is reds
+
+    std::vector<int> legalActions = boardState.getLegalActions();
+    this->childrenStarts[parent] = nextFree;
+    for (int action: legalActions) {
+        createNode(parent, action);
+    }
+}
+
+void MCTSSearcher::createNode(int parent, int action) {
+    this->parents[nextFree] = parent;
+    this->childActions[nextFree] = action;
+    this->childrenEnds[parent] = nextFree;
+    this->isREDs[nextFree] = !this->isREDs[parent];
+    nextFree++;
+}
+
+void MCTSSearcher::mainLoop(Hex boardState) {
+    long start = 0l;
+    int max_sims = 100000;
+    GameState gameState(boardState); // Create gameState via boardState
+
+    int i = 0;
+    while (i < max_sims) {
+        forward(gameState.clone());
+        i++;
+    }
+}
+
+
+void MCTSSearcher::forward(GameState gameState) {
+    double reward = 0;
+    int node = 0;
+    while (expandeds[node] && !terminals[node]) {
+        // select action via UCT
+        int childrenStart = childrenStarts[node];
+        int childrenEnd = childrenEnds[node]; // inclusive
+
+        int bestChildNode = childrenStart;
+        double bestChildUCT = -999999;
+        double logVisitCounts = log(this->visitCounts[node]);
+        for (int childNode = childrenStart; childNode <= childrenEnd; childNode++) {
+            double childQ = this->returnSums[childNode] / (0.0001 + this->visitCounts[childNode]);
+            double childUCT = childQ + 1.41 * sqrt(logVisitCounts / (0.0001 + this->visitCounts[childNode]));
+            if (childUCT > bestChildUCT) {
+                bestChildUCT = childUCT;
+                bestChildNode = childNode;
+            }
+        }
+
+        int action = this->childActions[bestChildNode];
+        gameState.makeMove(action);
+        node = bestChildNode;
+    }
+
+    // either node is not expanded or it is terminal here
+    if (!expandeds[node]) {
+        expand(node, gameState);
+    }
+    if (!terminals[node]) {
+        reward = rollout(gameState);
+    } else {
+        reward = gameState.getTerminalValue(true);
+    }
+    backup(reward, node);
+}
+
+double MCTSSearcher::rollout(GameState gameState) {
+    return 0;
+}
+
+
+void MCTSSearcher::backup(double reward, int artificialLeafNode) {
+    // reward is from red player's perspective
+    int node = artificialLeafNode;
+    while (node != -1) {
+        int parent = parents[node];
+        if (isREDs[node]) {
+            returnSums[node] += reward;
+        } else {
+            returnSums[node] -= reward;
+        }
+        visitCounts[node] += 1;
+
+        // apply reward and invert for
+        node = parent;
+    }
+}
--- MCTSSearcher.h
+++ MCTSSearcher.h
@@ -5,9 +5,43 @@
 #ifndef MCTSHEX_MCTSSEARCHER_H
 #define MCTSHEX_MCTSSEARCHER_H
 
+#define MAX_NODES 100000
+#include "GameState.h"
+#include "hex.h"
 
 class MCTSSearcher {
-    
+public:
+    int visitCounts[MAX_NODES]{};
+    double returnSums[MAX_NODES]{};
+    int parents[MAX_NODES]{};
+    int childrenStarts[MAX_NODES]{};
+    int childrenEnds[MAX_NODES]{};
+    bool expandeds[MAX_NODES]{};
+    bool terminals[MAX_NODES]{};
+    // mapping from parent to child node which action was taken to get from parent to child
+    int childActions[MAX_NODES]{};
+    bool isREDs[MAX_NODES]{};
+    int nextFree;
+
+    MCTSSearcher() {
+        nextFree = 0;
+    }
+
+    int search(Hex boardState);
+
+    int search(Hex boardState, bool isRED);
+
+    void expand(int parent, GameState gameState);
+
+    void createNode(int parent, int action);
+
+    void mainLoop(Hex boardState);
+
+    void forward(GameState gameState);
+
+    void backup(double reward, int artificialLeaf);
+
+    static double rollout(GameState gameState);
 };
 
 
