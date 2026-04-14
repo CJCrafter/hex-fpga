506520b0d42a6e76d569d228cb317ecc68a5b638
Aakash Kumar <aakashinthesky@gmail.com>
2026-04-14 11:06:00 -0400

    fix: lower problem size

--- MCTSSearcher.cpp
+++ MCTSSearcher.cpp
@@ -61,7 +61,7 @@ void MCTSSearcher::createNode(int parent, int action) {
 
 void MCTSSearcher::mainLoop(Hex boardState) {
     long start = 0l;
-    int max_sims = 2000;
+    int max_sims = 1;
     GameState gameState(boardState); // Create gameState via boardState
 
     int i = 0;
