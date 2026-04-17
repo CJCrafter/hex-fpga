9ec03b05c8dfd8e85104f3689cd841e96be0d202
Aakash Kumar <aakashinthesky@gmail.com>
2026-04-17 19:09:44 -0400

    fix: lowered num turns for faster runs

--- main.cpp
+++ main.cpp
@@ -44,31 +44,33 @@ void write_gamestate(hls::stream<bus_t> &strm_in, Hex<HEX_SIZE> hex, bool isRED)
     write_bitboard(strm_in, hex.player1());
     write_bitboard(strm_in, hex.player2());
 
-    strm_in.write((bus_t)isRED);
-    strm_in.write((bus_t)hex.isPlayer1Turn());
-    strm_in.write((bus_t)hex.is_first);
-    strm_in.write((bus_t)(hex.lastMove() + 1));
+    strm_in.write((bus_t) isRED);
+    strm_in.write((bus_t) hex.isPlayer1Turn());
+    strm_in.write((bus_t) hex.is_first);
+    strm_in.write((bus_t) (hex.lastMove() + 1));
 }
 
 int main() {
     Hex<HEX_SIZE> hex;
     bool isRED = true;
 
-    while (true) {
+    for (int turn = 0; turn < 3; turn++) {
         // MCTSSearcher *searcher = new MCTSSearcher(4l); // heap allocate
-        // auto t0 = std::chrono::high_resolution_clock::now();
         hls::stream<bus_t> dut_in;
         hls::stream<bus_t> dut_out;
 
         write_gamestate(dut_in, hex, isRED);
+        auto t0 = std::chrono::high_resolution_clock::now();
         dut(dut_in, dut_out);
+        int action = (int) dut_out.read();
 
-        int action = (int)dut_out.read();
-
-        // auto t1 = std::chrono::high_resolution_clock::now();
-        // double ms = std::chrono::duration<double, std::milli>(t1 - t0).count();
+        auto t1 = std::chrono::high_resolution_clock::now();
+        double ms = std::chrono::duration<double, std::milli>(t1 - t0).count();
         // delete searcher;
-        std::cout << "Made move " + std::to_string(action) << std::endl; // << " (" << ms << " ms)"
+        std::cout << "Made move " + std::to_string(action) << " (" << ms << " ms)" << std::endl; //
+        double sims_per_sec = 10000.0 / (ms / 1000.0);
+        std::cout << "sims/sec = " << sims_per_sec << "\n";
+
         hex.place(action);
         std::string player = isRED ? "Red" : "Blue";
         isRED = !isRED;
@@ -76,10 +78,10 @@ int main() {
         auto player1 = hex.player1();
         auto player2 = hex.player2();
         std::cout << "player1:";
-        printbits((unsigned long long)player1); // todo change this later
+        printbits((unsigned long long) player1); // todo change this later
         std::cout << std::endl;
         std::cout << "player2:";
-        printbits((unsigned long long)player2);
+        printbits((unsigned long long) player2);
         std::cout << std::endl;
 
         if (hex.checkWin()) {
