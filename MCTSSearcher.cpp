cd7a9216910f9dedf5d9013a4dc70ffc25decb47
Aakash Kumar <aakashinthesky@gmail.com>
2026-04-30 18:23:12 -0400

    added 4 parallel tree simulations

--- MCTSSearcher.cpp
+++ MCTSSearcher.cpp
@@ -285,6 +285,7 @@ int search(const Hex<HEX_SIZE> boardState, const bool isRED) {
     for (int i = 0; i < NUM_PRALLEL_ROOTS; i++) {
         boardStates[i] = Hex<HEX_SIZE>(boardState);
     }
+#pragma HLS ARRAY_PARTITION variable=boardStates type=complete dim=1
     for (int i = 0; i < NUM_PRALLEL_ROOTS; i++) {
 #pragma HLS UNROLL
         MCTSSearcher<MCTS_TOTAL_SIMS / NUM_PRALLEL_ROOTS> searcher(42l + i);
