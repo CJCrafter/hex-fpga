6464a9af0445eafe53fa63aea9874022fc677d44
Aakash Kumar <aakashinthesky@gmail.com>
2026-04-30 14:10:30 -0400

    added results.sh

--- make_results.sh
+++ make_results.sh
@@ -1,3 +1,14 @@
+if [ "$#" -ne 1 ]; then
+   echo "Usage: $0 <results_directory_name>"
+   exit 1
+fi
+
+if [ -d "results/$1" ]; then
+    echo "Directory results/$1 already exists. Please choose a different name."
+    exit 1
+fi
+
+
 mkdir results/$1
 cp kria results/$1/krea
 cp mcts.prj results/$1/krea
