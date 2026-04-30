c6554b40b631dd264fdb72318102145e9c12e564
Aakash Kumar <aakashinthesky@gmail.com>
2026-04-30 15:39:21 -0400

    added independent multiple roots

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 scp build/bitstream.bit *.cpp *.h ubuntu@10.210.1.175:/home/ubuntu/aakash27/hex-fpga/
-ssh ubuntu@10.210.1.175 "cd /home/ubuntu/aakash27/hex-fpga/ ; bash --login"
+ssh ubuntu@10.210.1.175
